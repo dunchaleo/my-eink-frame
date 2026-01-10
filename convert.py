9ba3209337463ede078f9de24785651316685574
dunchaleo <cmairusu@outlook.com>
2026-01-10 02:32:54 -0500

    structure for easy popup repl testing

--- convert.py
+++ convert.py
@@ -14,83 +14,81 @@ import os.path
 from PIL import Image, ImagePalette, ImageOps
 import argparse
 
-parser = argparse.ArgumentParser(description='--orientation: landscape/portrait  --mode: fill/fit/stretch  --background: light/dark')
+def main(f, o, m, b): #see args
+    if(b == 'light'):
+        display_background = (255,255,255)
+    else:
+        display_background = (0,0,0)
 
-# add and read orientation/mode args
-parser.add_argument('image_file', type=str, help='Input image file')
-parser.add_argument('--orientation', choices=['landscape', 'portrait'], help='Image direction (landscape or portrait)')
-parser.add_argument('--mode', choices=['fill', 'fit', 'stretch'], default='fit', help='Image framing mode (fill (crop), fit, or stretch (resize))')
-parser.add_argument('--background', choices=['light','dark'], default='light', help='Background color (light or dark) (only matters if \'--mode fit\')')
-args = parser.parse_args()
+    # Check whether the input file exists
+    if not os.path.isfile(f):
+        print(f'Error: file {f} does not exist')
+        sys.exit(1)
 
-# set globals from args
-input_filename = args.image_file
-display_orientation = args.orientation
-display_mode = args.mode
-if(args.background == 'light'):
-    display_background = (0,0,0)
-else:
-    display_background = (255,255,255)
+    # Read input image
+    input_image = Image.open(f)
 
-# Check whether the input file exists
-if not os.path.isfile(input_filename):
-    print(f'Error: file {input_filename} does not exist')
-    sys.exit(1)
-
-# Read input image
-input_image = Image.open(input_filename)
-
-# Get the original image size
-width, height = input_image.size
-
-# specify target size
-if display_orientation == 'landscape':
-    target_width, target_height = 800, 480
-elif display_orientation == 'portrait':
-    target_width, target_height = 480, 800
-
-#scale of source relative to target
-scale = (width / target_width, height / target_height)
-factor = min(scale[0],scale[1])
-
-# 4 cases:
-# source img x > target x,
-#  target x > source img x;
-# source img y > target y,
-#  target y > source img y.
-# a wide image whose dims are > 800,480 is a very common case for example.
-# we can take care of some edge cases by first ensuring source dims always >= target dims:
-if factor<1:
-    #directly change input_image, reset info. this shouldnt happen for most cases.
-    input_image = ImageOps.scale(input_image, 1/factor) #scale up
+    # Get the original image size
     width, height = input_image.size
+
+    # specify target size
+    if o == 'landscape':
+        target_width, target_height = 800, 480
+    elif o == 'portrait':
+        target_width, target_height = 480, 800
+
+    #scale of source relative to target
     scale = (width / target_width, height / target_height)
     factor = min(scale[0],scale[1])
 
-if display_mode == 'fill':
-    #crop from center
-    #NOTE this option is likely to be insufficent for any modern camera: it will be too zoomed in.
-    #  could do a trial-and-error repeated dims-check-and-adjust until right-ish upfront, then invoke pillow
-    left = (width - target_width) // 2
-    top = (height - target_height) // 2
-    right = (width + target_width) // 2
-    bottom = (height + target_height) // 2
-    target_image = input_image.crop((left,top,right,bottom))
-elif display_mode == 'fit':
-    content_scale = min(1/scale[0],1/scale[1])
-    temp_image = input_image.resize((int(width*content_scale),int(height*content_scale)), Image.Resampling.LANCZOS) #ImageOps.scale cant use fancy resampling
-    #padding to concat: padding = (target_height - temp_image.size[0], target_width - temp_image.size[1])
-    target_image = ImageOps.pad(temp_image, (target_width,target_height), color=display_background) #TODO test padding methods other than pad
-elif display_mode == 'stretch':
-    target_image = target_image.resize((target_width,target_height))
+    # 4 cases:
+    # source img x > target x,
+    #  target x > source img x;
+    # source img y > target y,
+    #  target y > source img y.
+    # a wide image whose dims are > 800,480 is a very common case for example.
+    # we can take care of some edge cases by first ensuring source dims always >= target dims:
+    if factor<1:
+        #directly change input_image, reset info. this shouldnt happen for most cases.
+        input_image = ImageOps.scale(input_image, 1/factor) #scale up
+        width, height = input_image.size
+        scale = (width / target_width, height / target_height)
+        factor = min(scale[0],scale[1])
+
+    if m == 'fill':
+        #crop from center
+        #NOTE this option is likely to be insufficent for any modern camera: it will be too zoomed in.
+        #  could do a trial-and-error repeated dims-check-and-adjust until right-ish upfront, then invoke pillow
+        left = (width - target_width) // 2
+        top = (height - target_height) // 2
+        right = (width + target_width) // 2
+        bottom = (height + target_height) // 2
+        target_image = input_image.crop((left,top,right,bottom))
+    elif m == 'fit':
+        content_scale = min(1/scale[0],1/scale[1])
+        temp_image = input_image.resize((int(width*content_scale),int(height*content_scale)), Image.Resampling.LANCZOS) #ImageOps.scale cant use fancy resampling
+        #padding to concat: padding = (target_height - temp_image.size[0], target_width - temp_image.size[1])
+        target_image = ImageOps.pad(temp_image, (target_width,target_height), color=display_background) #TODO test padding methods other than pad
+    elif m == 'stretch':
+        target_image = input_image.resize((target_width,target_height))
+
+    target_image.save("output.bmp")
+    print('Successfully converted?' )
 
 
-# The color quantization and dithering algorithms are performed, and the results are converted to RGB mode
-#quantized_image = resized_image.quantize(dither=display_dither, palette=pal_image).convert('RGB')
+parser = argparse.ArgumentParser(description='--orientation: landscape/portrait  --mode: fill/fit/stretch  --background: light/dark')
+# add and read orientation/mode args
+parser.add_argument('image_file', type=str, default='input.jpg', help='Input image file')
+parser.add_argument('--orientation', choices=['landscape', 'portrait'], help='Image direction (landscape or portrait)')
+parser.add_argument('--mode', choices=['fill', 'fit', 'stretch'], default='fit', help='Image framing mode (fill (crop), fit, or stretch (resize))')
+parser.add_argument('--background', choices=['light','dark'], default='light', help='Background color (light or dark) (only matters if \'--mode fit\')')
+#args = parser.parse_args()    # uncomment!!
 
-# Save output image
-#output_filename = os.path.splitext(input_filename)[0] + '_' + display_mode + '_output.bmp'
-#quantized_image.save(output_filename)
-target_image.save("output.bmp")
+#main(args.image_file, args.orientation, args.mode, args.background)
 
-print('Successfully converted?' )
+
+#notes misc
+#emacs: (pipenv-activate) found the venv correctly, comint repl just works
+#in python repl, `import sys\n print(sys.executable)`
+#`import filename` to source this file from repl
+#filename.main('input.jpg', 'landscape', 'fit', 'dark')
