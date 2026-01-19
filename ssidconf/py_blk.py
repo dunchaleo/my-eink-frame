13e9b55f209a3c9b9ee08916be9e6886a428dc2b
dunchaleo <>
2026-01-18 22:51:38 -0500

    small

--- ssidconf/py_blk.py
+++ ssidconf/py_blk.py
@@ -4,6 +4,6 @@ import os
 
 system = os.system("findmnt -n -o SOURCE") #/dev/mmcblk0 on rpi sdcard /
 
-names = open(".blk_devices").read().splitlines() #devices from last udev trigger
+names = open(".blk_devices.txt").read().splitlines() #devices from last udev trigger
 
 newnames = os.system("lsblk -d -o NAME,MOUNTPOINT").splitlines()
