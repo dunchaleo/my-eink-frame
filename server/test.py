0a393d8eeaf878682f728d8ceb7416ffe6d04c96
dunchaleo <>
2026-01-21 08:48:15 -0500

    write classes correctly

--- server/test.py
+++ server/test.py
@@ -13,36 +13,51 @@ app = Microdot()
 server_covnersion = True #convert files in browser or on device
 
 class Meta:
-    #row in files.csv has ``filename,timestamp'' (+ more cols?), see convert()
-    files = []
-    ordering = [] #array of indexes--this is what actually gets sorted!
-    sortby = 1 #default sorting is timestamp,
-    desc = False# (ascending)
-    def mwrite():
+    def __init__(self):
+        self.files = [] #row in files.csv has ``filename,timestamp'' (+ more cols?), see convert()
+        self.ordering = [] #array of indexes--this is what actually gets sorted!
+        self.sortby = 1 #default sorting is timestamp,
+        self.desc = False# (ascending)
+    def mwrite(self):
         with open('./files.csv', 'r') as f:
             reader = csv.reader(f)
             for row in reader:
                 self.files.append(row)
-    def fwrite():
+    def fwrite(self):
         with open('./files.csv', 'w') as f: #w mode clears file first
             writer = csv.writer(f)
             for row in self.files:
                 f.write(row)
-    def mfree():
+    def mfree(self):
         self.files = []
-        #self.ordering = []
-    def chsort(sortby, desc):
+        self.ordering = []
+    def chsort(self, sortby, desc):
         #could this could be optimized a lot?
-        ordering.sort(reverse=desc, key=lambda i: files[i])
+        self.ordering.sort(reverse=desc, key=lambda i: files[i])
         #also a setter
         self.sortby=sortby
-        self.asc=asc
-    def insert(newfile): #newfile looks just like a row of files.csv
+        self.desc=desc
+    def insert(self, newfile): #newfile looks just like a row of files.csv
         #see comment in ./converter/convert.py,
         #by expected use case insertion sort is probably really good here,
         #unless user picked from their recents the wrong way,
         #then comparison just needs to be reversed first (but how can you tell?)
+        for i in files:
+            if self.compare(newfile[sortby], files[i][sortby]):
+                pos += 1
+            else:
+                pos -= 1
 
+
+        fwrite()
+    def list_files(self):
+        #for(i=0; i<len; i++)
+        #  ret[i] = files[self.ordering[i]];
+        return [self.files[i] for i in self.ordering] #list comprensions are faster and take more ram
+    def close(self):
+        #this should be called in between server instances (writes csv in order)
+        self.files = self.list_files()
+        fwrite()
 
 #usage:
 #meta.chsort(1,True)
