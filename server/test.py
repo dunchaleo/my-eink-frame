1a4ec5a6d0a32d78b83397ac5751589e5c0d9dc7
dunchaleo <cmairusu@outlook.com>
2026-01-25 16:30:11 -0500

    fixed insertion
    using for x in range() was causing couting problems
    
    changed descending comparison to using complements and greaterthan

--- server/test.py
+++ server/test.py
@@ -49,26 +49,31 @@ class Meta:
         #by expected use case insertion sort is probably really good here,
         #unless user picked from their recents the wrong way,
         #then comparison just needs to be reversed first (but how can you tell?)
-        #   we could say if pos >= len-3 (or len-[tolerance]), we had a "hard" insert.
+        #   we could say if len-pos >= len-3 (or len-[tolerance]), we had a "hard" insert.
         #   more than 5 (or [also tolerance]?) hard inserts in a row means it might be worth ordering.reverse() and sortby=!sortby
         newlen = len(self.files)+1
-        self.files.append(newfile)
-        pos = newlen-1
-        for i in range(pos-1,-1,-1):#for(i=pos-1;i>=0;i--,pos--){..comp(,ordering[i]);..}
-            if self.compare(newfile, self.ordering[i]):
-                break
+        pos = newlen-2
+        while not self.compare(newfile,self.ordering[pos]):
             pos-=1
-        #insert real position into ordering after pos
+            #insert real position into ordering after pos
         self.ordering.append(0)
-        for i in range(newlen-1,pos+1,-1):
+        i = newlen-1
+        while (i>pos+1):
             self.ordering[i] = self.ordering[i-1]
-        self.ordering[pos+1] = newlen - 1
-        #fwrite() #insert really doesnt need this. fwrite is when user "applies changes", having the buffer open (self.files/ordering) means changes to file dont need to be made.
+            i-=1
+            self.ordering[pos+1] = newlen - 1
+            #ensure said real position is real
+        self.files.append(newfile)
+        #fwrite()
+        # ^ insert really doesnt need this. fwrite is when user "applies changes"; having the buffer open (self.files/ordering) means changes to file dont need to be made.
     def compare(self, new_elt, elt_idx):
         new_data = new_elt[self.sortby]
         elt_data = self.files[elt_idx][self.sortby]
         if(self.desc):
-            return (int(new_data)<int(elt_data)) #im stupid
+            max = int(self.files[0][self.sortby])
+            complement = max - int(elt_data)
+            new_complement = max - int(new_data)
+            return (new_complement > complement)
         else:
             return (int(new_data)>int(elt_data))
     def file_stream(self): #for(i=0; i<len; i++){ret[i] = files[self.ordering[i]];}
@@ -81,6 +86,7 @@ class Meta:
         #   so that encoded as a rule in a generator would be the same speed)
         fwrite()
         return self.files #useful
+
 #usage:
 #meta.insert(convert(file))
 #(inserts file into place based on timestamp)
