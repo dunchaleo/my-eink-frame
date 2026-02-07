f3e8b2ab0b907994e208f5d15819c5a2c210cdf4
dunchaleo <>
2026-02-07 01:57:21 -0500

    save

--- server/test.py
+++ server/test.py
@@ -45,39 +45,39 @@ async def upload(request):
             remaining -= len(chunk)
     print(f'{filename} uploaded and written')
 
-    asyncio.create_task(convert_and_insert(filename,size))
+    #fire converter task upon upload and immediately enque task handle
+    converter_handle = asyncio.create_task(process_image(filename))
+    await insertion_q.put(converter_handle)
 
-#WIP
-
-    meta.insert(future)
     return ''
-async def testconvert(filename, size):
-    # the point of this test is to show that if large filesize => long conversion time, the conversions will finish in the order of filesize.
-    # it's a bad test because task A could have longer run time than task B, but A returns first because it was fired first
-    time = (size/239024)*10 #normalized size (max is 239024) to 10
-    print(f'\t\t\t\t\t\t\t\033[92m async task started--{time}\033[0m')
-    await asyncio.sleep(time)
-    print(f'\t\t\t\t\t\t\t\033[91m async task returned: converted {filename} of size {size}\033[0m')
-async def convert_and_insert(filename, size):
-    time = (size/239024)*10 #normalized size (max is 239024) to 10
-    print(f'\t\t\t\t\t\t\t\033[92m async task started (debug: {time})\033[0m')
-    converter_handle = await asyncio.sleep(time) #await convert(filename) #conversion time
-    insertion_queue.put(converter_handle) #declare promise and enque it in the global insertion queue
 
-    print(f'\t\t\t\t\t\t\t\033[91m async task returned: converted {filename} of size {size}\033[0m')
-async def consume_staged_insertions():
-    while True:
-        nextfile = insertion_queue.get()
-        if(nextfile):
+async def insertion_listener(): #or "insertion consumer", from consumer/producer pattern
+   while True:
+       #exif = await (await insertion_q.get())
+       nexttask = await insertion_q.get()
+       metadata = (await nexttask)[meta.sortby] # how do i do what i want here
+async def process_image(filename):
+    #dont just get exif--let this task totally finish conversion
+    await converter.convert(filename)
+    exif = await converter.get_exif(filename)
+    return myexiftostr(exif) #format like row in files.csv
 
 
+
+
+# app.run(port=4000, debug=True)
+#microdot example/gpio.weather has usage of a bg task running concurrently with micodot app.
+async def start():
+    insertion_listener_bgtask = asyncio.create_task(insertion_listener())
+    server = asyncio.create_task(app.start_server(port=4000, debug=true))
+    await asyncio.gather(server,insertion_listener_bgtask)
 
 app = Microdot()
 
 meta = metadata.Meta()
-insertion_queue = asyncio.Queue()
+insertion_q = asyncio.Queue()
 
 Request.max_content_length = 1024*1024
 server_covnersion = True #convert files in browser or on device
 
-app.run(port=4000, debug=True)
+asyncio.run(start())
