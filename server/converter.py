d5521b44d9c091f222544d7520c1c755460cec9f
dunchaleo <>
2026-02-21 00:59:35 -0500

    converter update
    more specialized exif extraction to just a timestamp, might regret this

--- server/converter.py
+++ server/converter.py
@@ -1,9 +1,7 @@
 ### credits
 # conversion code modified from waveshare website
-# get_exif_bytes() modified from saschiwy/heic_converter
+# build_exif_dict and get_date() modified from saschiwy/heic_converter
 
-
-###
 #if we cant do server conversions, it means we're on a MCU that cant use pillow/advanced image library. since an MCU doesnt have an OS, we couldnt be calling a subprocess. so imo the converter is free to be a subprocess. it is also free to use thread pool executor. neither is available on micropython.
 
 #everyting in this file needs to be silent since it's subprocessed. debug logging has to be to a file.
@@ -20,34 +18,19 @@ def log(str):
     with open('.log','a+') as log:
         log.write(f'converter.py\t{str}')
 
-# (unique deps: piexif, datetime, ExifTags)
-# tries to copy exif via piexif first, but failing that (e.g. if png or tiff?) it still specifically extracts the datetime and builds its own exif dict.
-def get_exif_bytes(path, verbose:bool = False):
-    image = Image.open(path)
-    image_exif = image.getexif()
+#not really using this at all right now
+#might have been better fot converter.py subprocess to stdout this as a dict in raw bytes
+def build_exif_dict(image_info, date:datetime): #image.info attribute
 
+    # Try to load existing exif data via piexif
     exif_dict = {"0th": {}, "Exif": {}, "GPS": {}, "1st": {}}
+    try:
+        if "exif" in image_info:
+            exif_dict = piexif.load(image.info["exif"])
+    except:
+        # If loading fails, use our default structure
+        pass
 
-    if image_exif:
-        # Make a map with tag names and grab the datetime
-        exif = {ExifTags.TAGS[k]: v for k, v in image_exif.items() if k in ExifTags.TAGS and type(v) is not bytes}
-        if 'DateTime' in exif:
-            date = datetime.strptime(exif['DateTime'], '%Y:%m:%d %H:%M:%S')
-        else:
-            date = datetime.now()
-
-        # Try to load existing exif data via piexif
-        try:
-            if "exif" in image.info:
-                exif_dict = piexif.load(image.info["exif"])
-        except:
-            # If loading fails, use our default structure
-            pass
-    else:
-        # No EXIF data exists, use current datetime
-        date = datetime.now()
-        if verbose:
-            log(f'No EXIF data found for {image.filename}, creating dummy EXIF data')
     # Update exif data with orientation and datetime
     exif_dict["0th"][piexif.ImageIFD.DateTime] = date.strftime("%Y:%m:%d %H:%M:%S")
     exif_dict["0th"][piexif.ImageIFD.Orientation] = 1
@@ -59,22 +42,51 @@ def get_exif_bytes(path, verbose:bool = False):
     if not exif_dict.get("Exif"):
         exif_dict["Exif"] = {}
 
-    exif_bytes = piexif.dump(exif_dict)
-    return exif_bytes
+    #exif_bytes = piexif.dump(exif_dict)
+    #return exif_bytes
+    return exif_dict
 
-def exif_to_csv(path):
-   return string(get_exif_bytes)
+#for use in build_exif_dict and write_bytes
+def get_date(image_exif, verbose=False):
+     if image_exif:
+        # Make a map with tag names and grab the datetime
+        exif = {ExifTags.TAGS[k]: v for k, v in image_exif.items() if k in ExifTags.TAGS and type(v) is not bytes}
+        if 'DateTime' in exif:
+            date = datetime.strptime(exif['DateTime'], '%Y:%m:%d %H:%M:%S')
+        else:
+            date = datetime.now()
 
+    else:
+        # No EXIF data exists, use current datetime
+        date = datetime.now()
+        if verbose:
+            log(f'No EXIF data found for {image.filename}, creating dummy EXIF data')
+    return date
 
 #simulate an exif output ideally
-import sys
-import struct
-import time
-log(f'the input args are {sys.argv} (file, orientation, mode, background)')
-log('heres some blocking in the body\n')
-#stdout 255 byte chunk for filename, and unsigned int for timestamp. + more ?
-#doing this in C would mean stdout a char* and just know how to delimit it on the other side. so it's similar here:
-out_bytes = struct.pack('@255sI', b'filename.jpg',1770863679)
-sum(range(30000000*len(sys.argv[1])))
-log('this is the end of the blocking\n')
-sys.stdout.buffer.write(out_bytes)
+# import sys
+# import struct
+# import time
+# log(f'the input args are {sys.argv} (file, orientation, mode, background)')
+# log('heres some blocking in the body\n')
+# #stdout 255 byte chunk for filename, and unsigned int for timestamp. + more ?
+# #doing this in C would mean stdout a char* and just know how to delimit it on the other side. so it's similar here:
+# out_bytes = struct.pack('@255sI', b'filename.jpg',1770863679)
+# sum(range(30000000*len(sys.argv[1])))
+# log('this is the end of the blocking\n')
+# sys.stdout.buffer.write(out_bytes)
+
+#this makes sure Meta.insert() can read the output bytes and is separated like files.csv entry.
+def write_bytes(filename, date:datetime): #datetime + more for whatever other cols i decide to add
+    timestamp = date.timestamp()
+    out_bytes = struct.pack('@255sI',filename,timestamp)
+    sys.stdout.buffer.write(out_bytes)
+
+def main(f, o, m, b):
+    image = Image.open(f)
+    my_date = get_date(image.getexif(), True)
+    log(f'converting: {f} {o} {m} {b}\n')
+    write_bytes()
+
+if __name__ == '__main__':
+    main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
